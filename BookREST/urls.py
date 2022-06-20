c906153322b05419c57a6345c7276d577e5429b3
Dzianis <ulybqa@gmail.com>
2022-06-20 16:26:33 +0300

    add local static url

--- BookREST/urls.py
+++ BookREST/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 from rest_app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -11,4 +13,4 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
